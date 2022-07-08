9ec500430af63e3a20f1801a18efcc2e943e600e
Suyash <paradisestrings99@gmail.com>
2022-07-09 00:38:08 +0530

    add response html

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const http = require('http')
 const server = http.createServer((req, res) => {
     console.log(req)
     // process.exit()
+
+    res.setHeader("Content-Type", "text/html")
+    res.write(`
+        <html>
+            <head>
+                <title>My First Page</title>
+                <body>
+                    <h1>Hello From NodeJS Server!</h1>
+                </body>
+            </head>
+        </html>
+    `)
+    res.end()
 })
 
 server.listen(3000)
