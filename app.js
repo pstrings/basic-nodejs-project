68450f1af3bce571747f854437486f280bb3ae5e
Suyash <paradisestrings99@gmail.com>
2022-07-09 11:42:51 +0530

    added response to form data

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-    // process.exit()
+    // we are storing the url from the request in a variable
+    const url = req.url
+
+    if(url === "/") {
+        res.write(`
+            <html>
+                <head>
+                    <title>Enter Message</title>
+                    <body>
+                        <form action ="/message" method="POST">
+                            <input type="text" name="message">
+                            <button type="Submit">Send</button>
+                        </form>
+                    </body>
+                </head>
+            </html>
+        `)
+        // Here we are returning res.end because we don't want the res below if to be sent.
+        return res.end()
+    }
 
     res.setHeader("Content-Type", "text/html")
     res.write(`
