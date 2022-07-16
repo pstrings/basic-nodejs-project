d754c38dad1deb4be6904955e9c7f8f7c0b76bb3
Suyash <paradisestrings99@gmail.com>
2022-07-16 21:42:39 +0530

    created separate module to write requestHandler

--- app.js
+++ app.js
@@ -1,64 +1,7 @@
 const http = require("http")
-const fs = require("fs")
 
-const server = http.createServer((req, res) => {
-    // we are storing the url from the request in a variable
-    const url = req.url
-    const method = req.method
+const routes = require("./routes")
 
-    if(url === "/") {
-        res.write(`
-            <html>
-                <head>
-                    <title>Enter Message</title>
-                    <body>
-                        <form action="/message" method="POST">
-                            <input type="text" name="message">
-                            <button type="Submit">Send</button>
-                        </form>
-                    </body>
-                </head>
-            </html>
-        `)
-        // Here we are returning res.end because we don't want the res below if to be sent.
-        return res.end()
-    }
-
-    if (url === "/message" && method === "POST") {
-        const body = []
-        req.on("data", (chunk) => {
-          body.push(chunk)
-        })
-
-        return req.on("end", () => {
-            const parsedBody = Buffer.concat(body).toString()
-            //here we'll have the key value pair where key is "message" (because name="message" in input)
-            //value will be whatever we input. So it will contain "message=value" 
-
-            const message = parsedBody.split("=")[1]
-            fs.writeFile("message.txt", message, err => {
-                res.statusCode = 302
-                res.setHeader("Location", "/")
-                return res.end()
-            })
-        })
-
-        // This will do the same thing as statusCode + setHeader Location combined
-        // res.writeHead(302, {Location: "/"}
-    }
-
-    res.setHeader("Content-Type", "text/html")
-    res.write(`
-        <html>
-            <head>
-                <title>My First Page</title>
-                <body>
-                    <h1>Hello From NodeJS Server!</h1>
-                </body>
-            </head>
-        </html>
-    `)
-    res.end()
-})
+const server = http.createServer(routes)
 
 server.listen(3000)
